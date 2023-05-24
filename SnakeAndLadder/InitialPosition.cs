17e75e9d6ea2370a337da05b112b584ad908f561
Ganapati <ganapatasakatwa@gmail.com>
2023-05-24 07:03:20 +0530

    [GK] Player Position to check Ladder Snake and No Play

--- SnakeAndLadder/InitialPosition.cs
+++ SnakeAndLadder/InitialPosition.cs
@@ -8,6 +8,8 @@ namespace SnakeAndLadder
 {
     public class InitialPosition
     {
+        public const int ladder = 1;
+        public const int snake = 2;
         public  void dierollposition()
         {
             int position = 0;
@@ -18,6 +20,33 @@ namespace SnakeAndLadder
             Console.WriteLine("The Number Gets from Die Roll is --"+dieroll);
             position = dieroll;
             Console.WriteLine("The Position of Player After Die Roll is--"+position);
+
+            int option=random.Next(0, 3);
+            Console.WriteLine("The Option is--"+option);
+            switch(option)
+            {
+                case ladder:
+                    Console.WriteLine("The Option We get Ladder");
+                    position += dieroll;
+                    Console.WriteLine("New Position After Getting Ladder--"+position);
+                    break;
+                case snake:
+                    Console.WriteLine("The option player bite by Snake");
+                    position -= dieroll;
+                    Console.WriteLine("New Position After Bite by Snake--"+position);
+                    break;
+
+                    if(position<0)
+                    {
+                        position= 0;
+                        Console.WriteLine("The Player Start from Initial Position after Snake Bite--"+position);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("The option We got no Play and present Position is--"+position);
+                    break;
+            }
         }
     }
 }
