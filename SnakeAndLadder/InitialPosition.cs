b736e74dce940da7b54c4665cc2706277d2a3cfb
Ganapati <ganapatasakatwa@gmail.com>
2023-05-24 06:31:42 +0530

    [GK] Single Player initial Position

---
+++ SnakeAndLadder/InitialPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class InitialPosition
+    {
+        public  void initialposition()
+        {
+            int position = 0;
+            Console.WriteLine("Initial Position of Playe1 is--"+position);
+        }
+    }
+}
---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InitialPosition initialPosition=new InitialPosition();
+            initialPosition.initialposition();
+        }
+    }
+}
